586018a129ac7ea5d36d00b1effbecb69fd975bb
w8oh <sofia.privalova@mail.ru>
2023-10-31 23:44:57 +0300

    MainActivity small refactoring

--- app/src/main/java/ru/sonya/week3/MainActivity.kt
+++ app/src/main/java/ru/sonya/week3/MainActivity.kt
@@ -1,19 +1,16 @@
 package ru.sonya.week3
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.adapters.ItemAdapter
-import com.mikepenz.fastadapter.diff.FastAdapterDiffUtil
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var catList: List<FunCat>
-    private lateinit var catAdapter: catAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +23,9 @@ class MainActivity : AppCompatActivity() {
 
         val manager = LinearLayoutManager(this)
 
-        val catsTitles = mutableListOf<String>("Смешной кот", "Хихитбл кот", "Жесть смешной кот", "Котик хихи", "Забавный кот")
-        val catsSubtitles = mutableListOf<String>("хихихи", "хахахха", "АХАХ", "вахзпхв", "апхзвыхща")
-        val catsImages = mutableListOf<String>(
+        val catsTitles = listOf("Смешной кот", "Хихитбл кот", "Жесть смешной кот", "Котик хихи", "Забавный кот")
+        val catsSubtitles = listOf("хихихи", "хахахха", "АХАХ", "вахзпхв", "апхзвыхща")
+        val catsImages = listOf(
             "https://i.pinimg.com/564x/3c/e6/83/3ce6837f24627680bdd499bc8a2fb891.jpg",
             "https://i.pinimg.com/originals/af/ac/e8/aface8ae6a0c7f3d7c23f81e2ba16059.jpg",
             "https://i.pinimg.com/564x/e4/df/ca/e4dfca50c2dd6b4b1e59028676cc3cc6.jpg",
@@ -53,13 +50,13 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun catGenerator(count:Int, catsTitles: MutableList<String>, catsSubtitles: MutableList<String>, catsImages: MutableList<String>): MutableList<FunCat> {
+    fun catGenerator(count:Int, catsTitles: List<String>, catsSubtitles: List<String>, catsImages: List<String>): MutableList<FunCat> {
         val generatedList = mutableListOf<FunCat>()
         for (i in 1..count) {
             generatedList.add( FunCat(
-                catsImages[(0..(catsImages.size-1)).random()],
-                catsTitles[(0..(catsTitles.size-1)).random()],
-                catsSubtitles[(0..(catsSubtitles.size-1)).random()])
+                catsImages[(0..<catsImages.size).random()],
+                catsTitles[(0..<catsTitles.size).random()],
+                catsSubtitles[(0..<catsSubtitles.size).random()])
             )
         }
         return generatedList
